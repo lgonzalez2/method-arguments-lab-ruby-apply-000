10fff98e933fd23f1432ea6905f2393e42302001
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-15 01:27:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,3 +6,6 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+introduction("Josh")
+introduction_with_language("Dan", "Ember")
+
